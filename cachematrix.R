27598f6bab8652e749296b8ab56a19f641a7a864
waheed114 <waheed114@gmail.com>
2019-04-21 18:53:33 +0400

    Description of function

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,11 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
+## Caching the Inverse of a Matrix:
+## Matrix inversion is usually a costly computation and there may be some 
+## benefit to caching the inverse of a matrix rather than compute it repeatedly.
+## Below are a pair of functions that are used to create a special object that 
+## stores a matrix and caches its inverse.
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
